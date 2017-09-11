de1a4c00a769a3e2d5f70ac6b4d5af766aaa49ae
JoffreyPag <joffreypag@gmail.com>
2017-09-11 09:07:35 -0300

    aa

--- app/src/main/java/br/ufrn/eaj/tads/meuslivros/Main4Activity.java
+++ app/src/main/java/br/ufrn/eaj/tads/meuslivros/Main4Activity.java
@@ -6,12 +6,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class Main4Activity extends AppCompatActivity {
 
+    TextView nome, autor, ano, nota;
     ArrayAdapter<String> adaptador;
     List<Livro> livros;
     String[] titulos;
@@ -21,18 +23,30 @@ public class Main4Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
+        nome = (TextView)findViewById(R.id.textViewNome);
+        autor = (TextView)findViewById(R.id.textViewAutor);
+        ano = (TextView)findViewById(R.id.textViewAno);
+        nota = (TextView)findViewById(R.id.textViewNota);
+
         BancoHelper bh = new BancoHelper(this);
         livros = bh.findAll();
         preencheTitulos();
 
-        AutoCompleteTextView autocompletelivro = (AutoCompleteTextView)findViewById(R.id.campoAltoCompletar);
+        final AutoCompleteTextView autocompletelivro = (AutoCompleteTextView)findViewById(R.id.campoAltoCompletar);
         adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titulos);
         autocompletelivro.setAdapter(adaptador);
 
-        autocompletelivro.setOnClickListener(new AdapterView.OnItemClickListener() {
+        autocompletelivro.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                for(Livro li: livros){
+                    if(adaptador.getItem(i).toString().equals(li.getNome())){
+                        nome.setText(li.getNome());
+                        autor.setText(li.getAutor());
+                        ano.setText(li.getAno());
+                        nota.setText(""+li.getNota());
+                    }
+                }
             }
         });
     }
