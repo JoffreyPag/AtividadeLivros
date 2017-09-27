a802ff4e48e5982f487d58fad9878a66dfd47504
JoffreyPag <joffreypag@gmail.com>
2017-09-27 08:38:57 -0300

    fim

--- app/src/main/java/br/ufrn/eaj/tads/meuslivros/LivroRecyclerViewAdapter.java
+++ app/src/main/java/br/ufrn/eaj/tads/meuslivros/LivroRecyclerViewAdapter.java
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -27,7 +29,7 @@ public class LivroRecyclerViewAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //cria uma view e infla com um layout feito pra isso
-        View view = LayoutInflater.from(contexto).inflate(R.layout.livro_card_layout_inflater, parent, false);
+        View view = LayoutInflater.from(contexto).inflate(R.layout.livro_inflater_frame, parent, false);
         //chama o holder que extende RecycleView.Holder (ainda nesse arquivo) e seta a view
         LivroViewHolder holder = new LivroViewHolder(view);
         return holder;
@@ -44,9 +46,9 @@ public class LivroRecyclerViewAdapter extends RecyclerView.Adapter {
         Livro livroescolhido = listaLivros.get(position);
         //seta as informações do objeto nas views
         livroholder.titulo.setText(livroescolhido.getNome());
-        livroholder.autor.setText(livroescolhido.getAutor());
+        /*livroholder.autor.setText(livroescolhido.getAutor());
         livroholder.ano.setText(livroescolhido.getAno());
-        livroholder.nota.setText(String.valueOf(livroescolhido.getNota()));
+        livroholder.nota.setText(String.valueOf(livroescolhido.getNota()));*/
 
     }
 
@@ -55,15 +57,24 @@ public class LivroRecyclerViewAdapter extends RecyclerView.Adapter {
         return listaLivros == null ? 0:listaLivros.size();
     }
 
-    //o holder
+    //O HOLDER!!!
     public class LivroViewHolder extends RecyclerView.ViewHolder{
-        final TextView titulo, autor, ano, nota;
+        final TextView titulo;
+        //final TextView, autor, ano, nota;
+        final LinearLayout layoutNormal, layoutGone;
+        final Button undoButton;
         public LivroViewHolder(View v) {
             super(v);
-            titulo = (TextView) v.findViewById(R.id.tituloLista);
-            autor = (TextView) v.findViewById(R.id.autorLista);
-            ano = (TextView) v.findViewById(R.id.anoLista);
-            nota = (TextView) v.findViewById(R.id.notaLista);
+            titulo = (TextView) v.findViewById(R.id.nomeLivro);
+
+            //autor = (TextView) v.findViewById(R.id.autorLista);
+            //ano = (TextView) v.findViewById(R.id.anoLista);
+            //nota = (TextView) v.findViewById(R.id.notaLista);
+
+
+            layoutNormal = v.findViewById(R.id.layout_normal);
+            layoutGone = v.findViewById(R.id.layout_gone);
+            undoButton = v.findViewById(R.id.undo_button);
         }
     }
 
